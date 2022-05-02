7826bb20cc0dd5a2443300a559bb3d6d720da693
James Keicher <jamesdkeicher@gmail.com>
2022-05-02 16:33:56 -0700

    test: first test setup to verify loading text

---
+++ src/App.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+
+describe('App should fetch character data from Rick and Morty API', () => {
+  it('Should fetch and display a list characters to App', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    screen.getByText(/loading.../i);
+  });
+});
